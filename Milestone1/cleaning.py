4f886d09023cda890c183f26695d7a2de077ea0f
nihaal5cw <balgurinihalrao@gmail.com>
2026-03-15 13:26:20 +0530

    Update cleaning.py

--- Milestone1/cleaning.py
+++ Milestone1/cleaning.py
@@ -1,16 +1,9 @@
 import re
 
 def clean_text(text):
-    text = re.sub(r'\s+', ' ', text)
-    text = re.sub(r'[^\w\s.,]', '', text)
-    return text.strip()
 
-if __name__ == "__main__":
-    from extract import extract_text_from_pdf
+    text = re.sub(r'\s+', ' ', text)   # remove extra spaces
+    text = re.sub(r'[^\w\s.,!?]', '', text)  # remove special chars
+    text = text.strip()
 
-    file_path = "sample.pdf"
-    raw_text = extract_text_from_pdf(file_path)
-    cleaned = clean_text(raw_text)
-
-    print("\nCleaned Text (First 1000 characters):\n")
-    print(cleaned[:1000])
+    return text
